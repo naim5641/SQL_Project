9393353cdccc2b3e34615a7576521d0a52e22b71
MD NAIM UDDIN <148058479+naim5641@users.noreply.github.com>
2025-04-23 21:13:10 +0600

    Add files via upload

---
+++ Moderate - 1.sql
@@ -0,0 +1,22 @@
+select distinct email,first_name,last_name
+from customer
+join invoice on customer.customer_id = invoice.customer_id
+join invoice_line on invoice.invoice_id = invoice_line.invoice_id
+where track_id in(
+	select track_id from track
+	join genre on track.genre_id = genre.genre_id
+	where genre.name like 'Rock'
+)
+order by email;
+
+select distinct email,first_name,last_name
+from customer
+join invoice on customer.customer_id = invoice.customer_id
+join invoice_line on invoice.invoice_id = invoice_line.invoice_id
+join track on invoice_line.track_id = track.track_id
+join genre on track.genre_id = genre.genre_id
+
+where genre.genre_id = '1'
+order by email
+
+select * from track
